c28fcab3ca661078bb5777e754f06392e4a9a0ad
NilD4nil <119331691+NilD4nil@users.noreply.github.com>
2023-09-25 20:44:38 +0400

    Add files via upload

---
+++ MVC123/app/config/db.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'provider' => 'pgsql',
+    'hostname' => '194.190.152.134',
+    'database' => 'databaseone',
+    'username' => 'danmot',
+    'password' => 'phprojectPass0205'
+];
